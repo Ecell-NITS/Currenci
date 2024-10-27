6f05d72e4eb2168609c2ad5a72d0fb23228b469b
Bishal Das <dasbishal1717@gmail.com>
2024-10-28 00:17:39 +0530

    Fourth Commit

--- src/app/api/v1/getUser/route.ts
+++ src/app/api/v1/getUser/route.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import UserModel from "../../../../model/User";
 import { getDataFromToken } from "../../../../helpers/getDataFromToken";
+import dbConnect from "../../../../lib/dbConnect";
 
 export async function GET(request: NextRequest) {
   try {
+    await dbConnect();
     const userId = await getDataFromToken(request);
     const user = await UserModel.findOne({ _id: userId }).select("-password");
+    console.log(user);
     return NextResponse.json(user);
   } catch (err) {
     return NextResponse.json({ error: err.message }, { status: 500 });
