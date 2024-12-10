1a5cf1dd7c27fadf69214486cb475edf1f2c1161
A-nirvana <anirban_ug_23@gmail.com>
2024-12-10 14:39:15 +0530

    Landing Page initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,21 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "angled-gradient": "linear-gradient(120deg, white, #1E3432, #999)",
+      },
+      animation: {
+        slideRight: "slideRight 0.5s ease-in-out",
+        slideLeft: "slideLeft 0.5s ease-in-out",
+      },
+      keyframes: {
+        slideRight: {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        slideLeft: {
+          "0%": { transform: "translateX(100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
       },
     },
   },
