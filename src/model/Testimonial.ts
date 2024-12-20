0406b0bc19bef13cffbdfa018d6f3631a4e82dcd
A-nirvana <anirban_ug_23@gmail.com>
2024-12-20 08:13:08 +0530

    moment timezone fix

--- src/model/Testimonial.ts
+++ src/model/Testimonial.ts
@@ -1,23 +1,41 @@
 import mongoose, { Document, Schema } from "mongoose";
+import moment from "moment-timezone";
+import type { User } from "./User";
 
 export interface ITestimonial extends Document {
-  userId: string;
   content: string;
-  isApproved: boolean;
+  givenBy: User;
   createdAt: Date;
+  isApproved: boolean;
   updatedAt: Date;
 }
 
-const TestimonialSchema: Schema = new Schema(
-  {
-    userId: { type: String, required: true },
-    content: { type: String, required: true },
-    isApproved: { type: Boolean, default: false },
+const TestimonialSchema: Schema = new Schema({
+  content: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
+  givenBy: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
   },
-);
+  createdAt: {
+    type: Date,
+    required: true,
+    default: () => moment().tz("Asia/Kolkata").toDate(),
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: () => moment().tz("Asia/Kolkata").toDate(),
+  },
+  isApproved: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
 
 export default mongoose.models.Testimonial ||
   mongoose.model<ITestimonial>("Testimonial", TestimonialSchema);
