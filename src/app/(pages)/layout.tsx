d98c84e4d84bcefc6017423a15bd0f90fb5a79c2
Bishal Das <dasbishal1717@gmail.com>
2025-01-19 12:29:36 +0530

    Team Management Admin

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,12 +1,27 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
+  // Paths where Navbar and Footer should be hidden
+  const noLayoutPaths = [
+    "/login",
+    "/signup",
+    "/resetPassword",
+    "/admin/addTeamMember",
+    "/admin",
+  ];
+
+  const shouldHideLayout = noLayoutPaths.includes(pathname);
   return (
     <main>
-      <Navbar />
+      {!shouldHideLayout && <Navbar />}
       {children}
-      <Footer />
+      {!shouldHideLayout && <Footer />}
     </main>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const url = new URL(request.url);
   const path = url.pathname;
 
   const token = request.cookies.get("signInToken")?.value || "";
+  const adminToken = request.cookies.get("adminToken")?.value || "";
 
   if (token && (path === "/signIn" || path === "/signUp")) {
     const response = NextResponse.redirect(new URL("/", url));
@@ -17,9 +18,19 @@ export function middleware(request: NextRequest) {
     return response;
   }
 
+  if (!adminToken && (path === "/admin" || path === "/admin/addTeamMember")) {
+    const response = NextResponse.redirect(new URL("/", url));
+    response.cookies.set("toastMessage", "Admin Access Required", {
+      httpOnly: false,
+      path: "/",
+      maxAge: 60 * 5,
+    });
+    return response;
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/signIn", "/signUp"],
+  matcher: ["/", "/signIn", "/signUp", "/admin", "/admin/addTeamMember"],
 };
