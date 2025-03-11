53bc06a25c953bc9e3c8a6e8108e89c079cc49ee
A-nirvana <anirban_ug_23@gmail.com>
2025-03-11 09:57:37 +0530

    Testimonial fixes

--- src/app/(pages)/admin/testimonials/page.tsx
+++ src/app/(pages)/admin/testimonials/page.tsx
@@ -26,6 +26,7 @@ const Testimonials = () => {
   const [disp, setDisp] = useState<ITestimonial[]>([]);
   const [current, setCurrent] = useState<ITestimonial | null>(null);
   const [publishing, setPublishing] = useState(false);
+  const [deleting, setDeleting] = useState(false);
   const router = useRouter();
   const options = [
     "Loved it!",
@@ -38,7 +39,6 @@ const Testimonials = () => {
     setPublishing(true);
     const resp = await fetch("/api/v1/getUser");
     const info = await resp.json();
-    console.log(info);
     if (info.role !== "admin" && info.role !== "superadmin") {
       toast.error("You're not authenticated");
       setTimeout(() => {
@@ -47,44 +47,143 @@ const Testimonials = () => {
       }, 1000);
       return;
     }
-    fetch(`/api/v1/approveTestimonial?Id=${id}`, {
+    const res = await fetch(`/api/v1/approveTestimonial?Id=${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-    })
-      .then((res) => {
-        if (res.status === 403) {
-          toast.error("You're not autorized");
-          return;
-        }
-        res.json();
-      })
-      .then(() => {
-        const x = test.filter((item) => item._id !== id);
-        toast.success("Published successfully", {
-          position: "top-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          theme: "colored",
-          style: {
-            backgroundColor: "#1E3432",
-            maxWidth: "90%",
-            padding: "10px",
-            margin: "10px",
-            width: "200px",
-            fontSize: "16px",
-            color: "#ffffff",
-          },
-        });
+    });
+    if (res.status === 403) {
+      toast.error("You're not authorized");
+      return;
+    }
+    if (res.status === 500) {
+      toast.error("Something went wrong");
+      return;
+    }
+    const data = await res.json();
+    toast.success("Published successfully", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "colored",
+      style: {
+        backgroundColor: "#1E3432",
+        maxWidth: "90%",
+        padding: "10px",
+        margin: "10px",
+        width: "200px",
+        fontSize: "16px",
+        color: "#ffffff",
+      },
+    });
+    setPublishing(false);
+    setTest(data);
+    setDisp(data.filter((item: ITestimonial) => !item.isApproved));
+    setCurrent(null);
+  };
+
+  const deleti = async (id) => {
+    setDeleting(true);
+    const resp = await fetch("/api/v1/getUser");
+    const info = await resp.json();
+    if (info.role !== "admin" && info.role !== "superadmin") {
+      toast.error("You're not authenticated");
+      setTimeout(() => {
+        setDeleting(false);
+      }, 1000);
+      return;
+    }
+    const res = await fetch(`/api/v1/deleteTestimonial?Id=${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.status === 403) {
+      toast.error("You're not authorized");
+      return;
+    }
+    if (res.status === 500) {
+      toast.error("Something went wrong");
+      return;
+    }
+    const data = await res.json();
+    toast.success("Deleted successfully", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "colored",
+      style: {
+        backgroundColor: "#1E3432",
+        maxWidth: "90%",
+        padding: "10px",
+        margin: "10px",
+        width: "200px",
+        fontSize: "16px",
+        color: "#ffffff",
+      },
+    });
+    setDeleting(false);
+    setTest(data);
+    setDisp(data.filter((item: ITestimonial) => !item.isApproved));
+    setCurrent(null);
+  };
+  const unpublish = async (id) => {
+    setPublishing(true);
+    const resp = await fetch("/api/v1/getUser");
+    const info = await resp.json();
+    if (info.role !== "admin" && info.role !== "superadmin") {
+      toast.error("You're not authenticated");
+      setTimeout(() => {
         setPublishing(false);
-        setTest(x);
-        setDisp(x);
-        setCurrent(null);
-      });
+        router.push("/");
+      }, 1000);
+      return;
+    }
+    const res = await fetch(`/api/v1/unpublishTestimonial?Id=${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.status === 403) {
+      toast.error("You're not authorized");
+      return;
+    }
+    if (res.status === 500) {
+      toast.error("Something went wrong");
+      return;
+    }
+    const data = await res.json();
+    toast.success("Unpublished successfully", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "colored",
+      style: {
+        backgroundColor: "#1E3432",
+        maxWidth: "90%",
+        padding: "10px",
+        margin: "10px",
+        width: "200px",
+        fontSize: "16px",
+        color: "#ffffff",
+      },
+    });
+    setPublishing(false);
+    setTest(data);
+    setDisp(data.filter((item: ITestimonial) => !item.isApproved));
+    setCurrent(null);
   };
 
   useEffect(() => {
@@ -92,7 +191,7 @@ const Testimonials = () => {
       const res = await fetch(`/api/v1/getAllTestimonial`);
       const data = await res.json();
       const arr = data.filter((item: ITestimonial) => !item.isApproved);
-      setTest(arr);
+      setTest(data);
       setDisp(arr);
     };
     fetchTest();
@@ -103,10 +202,10 @@ const Testimonials = () => {
       className="bg-white w-screen min-h-screen flex flex-col pb-16"
       style={{ fontFamily: "Sofia Pro Light" }}
     >
-      <div className="w-full px-[13vw] mt-44 mb-20">
+      <div className="w-full px-[13vw] mt-36 mb-20">
         <div className="flex gap-4">
           <button
-            onClick={() => setDisp(test)}
+            onClick={() => setDisp(test.filter((item) => !item.isApproved))}
             className="bg-[#1E3432] text-white px-4 py-2 flex gap-2 items-center hover:cursor-pointer"
           >
             <p>Total Reviews</p>
@@ -115,21 +214,44 @@ const Testimonials = () => {
             </span>
           </button>
           <button
-            onClick={() => setDisp(test.filter((item) => item.rating > 3))}
+            onClick={() =>
+              setDisp(
+                test.filter((item) => item.rating > 3 && !item.isApproved),
+              )
+            }
             className="bg-[#1E3432] text-white px-4 py-2 flex gap-2 items-center hover:cursor-pointer"
           >
             <p>Good Reviews</p>
             <span className="rounded-full px-2 aspect-square flex justify-center items-center bg-white text-black">
-              {test ? test.filter((item) => item.rating > 3).length : 0}
+              {test
+                ? test.filter((item) => item.rating > 3 && !item.isApproved)
+                    .length
+                : 0}
             </span>
           </button>
           <button
-            onClick={() => setDisp(test.filter((item) => item.rating < 3))}
+            onClick={() =>
+              setDisp(
+                test.filter((item) => item.rating < 3 && !item.isApproved),
+              )
+            }
             className="bg-[#1E3432] text-white px-4 py-2 flex gap-2 items-center cursor-pointer"
           >
             <p>Poor Reviews</p>
             <span className="rounded-full px-2 aspect-square flex justify-center items-center bg-white text-black">
-              {test ? test.filter((item) => item.rating < 3).length : 0}
+              {test
+                ? test.filter((item) => item.rating < 3 && !item.isApproved)
+                    .length
+                : 0}
+            </span>
+          </button>
+          <button
+            onClick={() => setDisp(test.filter((item) => item.isApproved))}
+            className="bg-[#1E3432] text-white px-4 py-2 flex gap-2 items-center hover:cursor-pointer"
+          >
+            <p>Published Reviews</p>
+            <span className="rounded-full px-2 aspect-square flex justify-center items-center bg-white text-black">
+              {test ? test.filter((item) => item.isApproved).length : 0}
             </span>
           </button>
         </div>
@@ -143,26 +265,22 @@ const Testimonials = () => {
           <div className="overflow-scroll flex flex-col gap-4 h-[60vh]">
             {disp.map((item) => {
               return (
-                !item.isApproved && (
-                  <div
-                    role="button"
-                    tabIndex={0}
-                    key={item.username + item.createdAt.toString()}
-                    onClick={() => setCurrent(item)}
-                    className="border-[#D1D1D1] border rounded-md p-4 pb-6 relative hover:cursor-pointer text-sm"
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter" || e.key === " ") setCurrent(item);
-                    }}
-                  >
-                    <p>{options[5 - item.rating]}</p>
-                    <p style={{ fontFamily: "Sofia Pro Ultralight" }}>
-                      {truncateText(item.content, 175)}
-                    </p>
-                    <p className="absolute bottom-0 right-2">
-                      ~{item.username}
-                    </p>
-                  </div>
-                )
+                <div
+                  role="button"
+                  tabIndex={0}
+                  key={item.username + item.createdAt.toString()}
+                  onClick={() => setCurrent(item)}
+                  className="border-[#D1D1D1] border rounded-md p-4 pb-6 relative hover:cursor-pointer text-sm"
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter" || e.key === " ") setCurrent(item);
+                  }}
+                >
+                  <p>{options[5 - item.rating]}</p>
+                  <p style={{ fontFamily: "Sofia Pro Ultralight" }}>
+                    {truncateText(item.content, 175)}
+                  </p>
+                  <p className="absolute bottom-0 right-2">~{item.username}</p>
+                </div>
               );
             })}
           </div>
@@ -198,18 +316,45 @@ const Testimonials = () => {
               </p>
             </div>
           )}
-          {current && (
-            <button
-              onClick={() => {
-                approve(current._id);
-              }}
-              disabled={publishing}
-              style={{ fontFamily: "Sofia Pro Medium" }}
-              className={`cursor-pointer px-4 py-1 bg-[#14342F] text-xl mt-4 text-white rounded-full border border-[#F2B263] ${publishing ? "opacity-75" : ""}`}
-            >
-              {publishing ? "Publishing" : "Publish"}
-            </button>
-          )}
+          <div className="gap-8 flex">
+            {current && current.isApproved && (
+              <button
+                onClick={() => {
+                  unpublish(current._id);
+                }}
+                disabled={publishing}
+                style={{ fontFamily: "Sofia Pro Medium" }}
+                className={`cursor-pointer px-4 py-1 bg-[#14342F] text-xl mt-4 text-white rounded-full border border-[#F2B263] ${publishing ? "opacity-75" : ""}`}
+              >
+                {publishing ? "Unpublishing" : "Unpublish"}
+              </button>
+            )}
+            {current && !current.isApproved && (
+              <button
+                onClick={() => {
+                  approve(current._id);
+                }}
+                disabled={publishing}
+                style={{ fontFamily: "Sofia Pro Medium" }}
+                className={`cursor-pointer px-4 py-1 bg-[#14342F] text-xl mt-4 text-white rounded-full border border-[#F2B263] ${publishing ? "opacity-75" : ""}`}
+              >
+                {publishing ? "Publishing" : "Publish"}
+              </button>
+            )}
+
+            {current && (
+              <button
+                onClick={() => {
+                  deleti(current._id);
+                }}
+                disabled={deleting}
+                style={{ fontFamily: "Sofia Pro Medium" }}
+                className={`cursor-pointer px-4 py-1 bg-[#780000] text-xl mt-4 text-white rounded-full border border-[#F2B263] ${deleting ? "opacity-75" : ""}`}
+              >
+                {deleting ? "Deleting" : "Delete"}
+              </button>
+            )}
+          </div>
         </div>
       </section>
     </main>
